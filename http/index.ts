09aeed07c9a81b7a6bbab54c73897dff81bc2263
warmergroup <ulugbekjonsultonov4@gmail.com>
2025-07-26 10:45:13 +0500

    update https to http

--- http/index.ts
+++ http/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'https://api.toyxonatop.uz'
+export const API_URL = 'http://api.toyxonatop.uz'
 
 const $axios = axios.create({
     baseURL: `${API_URL}/api`,
